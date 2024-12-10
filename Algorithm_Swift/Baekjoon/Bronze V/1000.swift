be0b0bbce2d7f1e0895aa6797cb32f5cafbd2890
DongHyeonDev <dhkim960206@gmail.com>
2024-12-11 08:54:30 +0900

    Fix: Baekjoon/1000

--- Algorithm_Swift/Baekjoon/Bronze V/1000.swift
+++ Algorithm_Swift/Baekjoon/Bronze V/1000.swift
@@ -7,5 +7,24 @@
 
 import Foundation
 
-let input = readLine()!.split(separator: " ").map { Int($0)! }
-print(input[0] + input[1])
+// 입력 받기
+if let input = readLine() {
+    // 입력 문자열을 공백 기준으로 나누고 정수로 변환
+    let numbers = input.split(separator: " ").compactMap { Int($0) }
+    if numbers.count == 2 {
+        let a = numbers[0]
+        let b = numbers[1]
+        
+        // 조건 확인: 0 < A, B < 10
+        if a > 0 && b > 0 && a < 10 && b < 10 {
+            // 출력
+            print(a + b)
+        } else {
+            print("입력 값은 0보다 크고 10보다 작은 정수여야 합니다.")
+        }
+    } else {
+        print("두 개의 정수를 입력해주세요.")
+    }
+} else {
+    print("입력이 잘못되었습니다.")
+}
