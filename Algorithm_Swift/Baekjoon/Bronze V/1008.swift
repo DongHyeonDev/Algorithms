8fdb77f10df992af880c51e2b46bb667bc65eba8
DongHyeonDev <dhkim960206@gmail.com>
2024-12-10 23:49:19 +0900

    Feat: Baekjoon/1008

---
+++ Algorithm_Swift/Baekjoon/Bronze V/1008.swift
@@ -0,0 +1,11 @@
+//
+//  1008.swift
+//  Algorithm_Swift
+//
+//  Created by 김동현 on 12/10/24.
+//
+
+import Foundation
+
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+print(Double(input[0]) / Double(input[1]))
