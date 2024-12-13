8074f8e35d5a71b620757f26eeb418407a5b0965
DongHyeonDev <dhkim960206@gmail.com>
2024-12-13 13:11:07 +0900

    Feat: Baekjoon/25083

---
+++ Algorithm_Swift/Baekjoon/Bronze V/25083.swift
@@ -0,0 +1,15 @@
+//
+//  25083.swift
+//  Algorithm_Swift
+//
+//  Created by 김동현 on 12/13/24.
+//
+
+import Foundation
+
+print("         ,r'\"7")
+print("r`-_   ,'  ,/")
+print(" \\. \". L_r'")
+print("   `~\\/")
+print("      |")
+print("      |")
