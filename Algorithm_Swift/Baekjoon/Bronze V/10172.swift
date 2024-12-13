d760a4e1db594aa0168baf687a4cf29d923ef9e9
DongHyeonDev <dhkim960206@gmail.com>
2024-12-13 13:07:00 +0900

    Feat: Baekjoon/10172

---
+++ Algorithm_Swift/Baekjoon/Bronze V/10172.swift
@@ -0,0 +1,14 @@
+//
+//  10172.swift
+//  Algorithm_Swift
+//
+//  Created by 김동현 on 12/13/24.
+//
+
+import Foundation
+
+print("|\\_/|")
+print("|q p|   /}")
+print("( 0 )\"\"\"\\")
+print("|\"^\"`    |")
+print("||_/=\\\\__|")
