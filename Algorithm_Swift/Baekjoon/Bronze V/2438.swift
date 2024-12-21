2c9454da74cc92d1d23b56f39ecea937248ed74a
DongHyeonDev <dhkim960206@gmail.com>
2024-12-21 23:32:56 +0900

    Feat: Baekjoon/2438

---
+++ Algorithm_Swift/Baekjoon/Bronze V/2438.swift
@@ -0,0 +1,23 @@
+//
+//  2438.swift
+//  Algorithm_Swift
+//
+//  Created by 김동현 on 12/21/24.
+//
+
+import Foundation
+
+// 입력 받기
+if let input = readLine(), let n = Int(input) {
+    // 조건 확인: 1 ≤ N ≤ 100
+    if n >= 1 && n <= 100 {
+        // 별 출력
+        for i in 1...n {
+            print(String(repeating: "*", count: i))
+        }
+    } else {
+        print("입력 값은 1 이상 100 이하의 정수여야 합니다.")
+    }
+} else {
+    print("올바른 정수를 입력해주세요.")
+}
