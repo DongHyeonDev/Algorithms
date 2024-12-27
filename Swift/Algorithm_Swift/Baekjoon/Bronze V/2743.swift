11d3062b4ade153944f86abef13c1d5332e57714
DongHyeonDev <dhkim960206@gmail.com>
2024-12-27 23:07:38 +0900

    Feat: Baekjoon/2743

---
+++ Swift/Algorithm_Swift/Baekjoon/Bronze V/2743.swift
@@ -0,0 +1,15 @@
+//
+//  2743.swift
+//  Algorithm_Swift
+//
+//  Created by 김동현 on 12/27/24.
+//
+
+import Foundation
+
+// 입력 받기
+if let input = readLine(), input.count <= 100 {
+    print(input.count)
+} else {
+    print("단어의 길이는 최대 100이어야 합니다.")
+}
