6ecf8f40984362e387bd81bccb1b281c4ecd36c0
DongHyeonDev <dhkim960206@gmail.com>
2024-12-28 18:31:59 +0900

    Feat: Baekjoon/2744

---
+++ Swift/Algorithm_Swift/Baekjoon/Bronze V/2744.swift
@@ -0,0 +1,25 @@
+//
+//  2744.swift
+//  Algorithm_Swift
+//
+//  Created by 김동현 on 12/28/24.
+//
+
+import Foundation
+
+// 입력 받기
+if let input = readLine(), input.count <= 100 {
+    // 대소문자 변환
+    let result = input.map { char -> Character in
+        if char.isLowercase {
+            return Character(char.uppercased())
+        } else if char.isUppercase {
+            return Character(char.lowercased())
+        }
+        return char
+    }
+    // 결과 출력
+    print(String(result))
+} else {
+    print("단어의 길이는 최대 100이어야 합니다.")
+}
