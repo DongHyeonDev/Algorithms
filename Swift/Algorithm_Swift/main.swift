d70f83a1f77a72383a97a97a47d2b9ef9f1c1ed3
DongHyeonDev <dhkim960206@gmail.com>
2024-12-28 23:49:14 +0900

    Feat: main.swift 파일 생성

---
+++ Swift/Algorithm_Swift/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Algorithm_Swift
+//
+//  Created by 김동현 on 12/28/24.
+//
+
+// Swift Version Info
+// 백준: Swift 5.5.2
+// 프로그래머스: Swift 5.2.5
+
+import Foundation
+
+let main = BOJ2744()
+main.run()
