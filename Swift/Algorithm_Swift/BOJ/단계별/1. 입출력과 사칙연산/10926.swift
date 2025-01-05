efc2f1dd24585aad4f4de9e389334dd6fa5dcef3
DongHyeonDev <dhkim960206@gmail.com>
2025-01-05 15:12:42 +0900

    Feat: BOJ10926 - ??!

---
+++ Swift/Algorithm_Swift/BOJ/단계별/1. 입출력과 사칙연산/10926.swift
@@ -0,0 +1,20 @@
+//
+//  10926.swift
+//  Algorithm_Swift
+//
+//  Created by 김동현 on 1/5/25.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/10926
+//  알고리즘 분류: 구현
+
+class BOJ10926: Solvable {
+    // 메모리: 79504KB, 시간: 8ms, 코드 길이: 123B
+    func run() {
+        // 입력 받기
+        if let input = readLine() {
+            // 출력: 입력값 + "??!"
+            print("\(input)??!")
+        }
+    }
+}
--- Swift/Algorithm_Swift/main.swift
+++ Swift/Algorithm_Swift/main.swift
@@ -11,5 +11,5 @@
 
 import Foundation
 
-let main = BOJ2475()
+let main = BOJ10926()
 main.run()
