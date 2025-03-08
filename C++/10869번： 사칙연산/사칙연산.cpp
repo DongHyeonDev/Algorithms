e51691f0b5cf25fd61ad72ee250064537598fb54
DongHyeonDev <dhkim960206@gmail.com>
2025-03-08 23:14:23 +0900

    Feat: [C++] BOJ10869 - 사칙연산

---
+++ C++/10869번： 사칙연산/사칙연산.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 10869                             :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: dhkim0206 <boj.kr/u/dhkim0206>              +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/10869                          #+#        #+#      #+#    */
+/*   Solved: 2025/03/08 23:07:11 by dhkim0206     ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+#include <iostream>
+int main() {
+    int A, B;
+    std::cin >> A >> B;
+    std::cout << A + B << std::endl;
+    std::cout << A - B << std::endl;
+    std::cout << A * B << std::endl;
+    std::cout << A / B << std::endl;
+    std::cout << A % B << std::endl;
+    return 0;
+}
