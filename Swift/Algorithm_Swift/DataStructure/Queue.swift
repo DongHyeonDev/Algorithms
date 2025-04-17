2145ecbd0d414668d527a59234d46a4c85a05487
DongHyeonDev <dhkim960206@gmail.com>
2025-04-17 20:43:12 +0900

    Feat: [Swift] DataStructure/Queue 수정

--- Swift/Algorithm_Swift/DataStructure/Queue.swift
+++ Swift/Algorithm_Swift/DataStructure/Queue.swift
@@ -8,15 +8,21 @@
 // 큐 자료구조 구현
 struct Queue {
     private var elements: [Int] = []
-    private var head = 0
+    private var head: Int = 0
+    
+    init() {}
+    
+    init(initialElement: Int) {
+        elements = [initialElement]
+    }
     
     // 큐에 요소 추가
-    mutating func push(_ x: Int) {
-        elements.append(x)
+    mutating func enqueue(_ element: Int) {
+        elements.append(element)
     }
     
     // 큐에서 요소 제거하고 반환
-    mutating func pop() -> Int {
+    mutating func dequeue() -> Int {
         if isEmpty() {
             return -1
         }
@@ -24,7 +30,7 @@ struct Queue {
         let value = elements[head]
         head += 1
         
-        // 큐의 앞부분이 너무 커지면 배열 최적화
+        // 메모리 최적화: head가 너무 커지면 사용하지 않는 부분 제거
         if head > 1000 {
             elements.removeFirst(head)
             head = 0
@@ -40,7 +46,7 @@ struct Queue {
     
     // 큐가 비어 있는지 확인
     func isEmpty() -> Bool {
-        return size() == 0
+        return head >= elements.count
     }
     
     // 큐의 맨 앞 요소 반환
