38156f2feb51cefe93254b853e6241cb16127c4f
DongHyeonDev <dhkim960206@gmail.com>
2025-07-23 22:04:16 +0900

    Feat: [C] BOJ2745 - 진법 변환

---
+++ C/2745번: 진법 변환/진법 변환.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 2745                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: dhkim0206 <boj.kr/u/dhkim0206>              +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/2745                           #+#        #+#      #+#    */
+/*   Solved: 2025/07/23 19:41:27 by dhkim0206     ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char Nstr[35];
+    int B;
+    scanf("%34s %d", Nstr, &B);
+
+    long long result = 0;
+    int len = strlen(Nstr);
+    for (int i = 0; i < len; i++) {
+        char c = Nstr[i];
+        int digit;
+        if (c >= '0' && c <= '9') {
+            digit = c - '0';
+        } else {
+            digit = c - 'A' + 10;
+        }
+        result = result * B + digit;
+    }
+
+    printf("%lld\n", result);
+    return 0;
+}
