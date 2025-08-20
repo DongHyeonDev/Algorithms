144da00cb0eb39e2b4d32c0fe1a1940c40077d4c
DongHyeonDev <dhkim960206@gmail.com>
2025-08-20 16:03:37 +0900

    Feat: RhynoFileIO - readIntOptional 함수 추가

--- Swift/Algorithm_Swift/Utility/RhynoFileIO.swift
+++ Swift/Algorithm_Swift/Utility/RhynoFileIO.swift
@@ -67,4 +67,24 @@ final class RhynoFileIO {
 
         return Array(buffer[beginIndex..<(index-1)])
     }
+    
+    @inline(__always) func readIntOptional() -> Int? {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        // 공백/개행 스킵
+        while now == 10 || now == 32 { now = read() }
+        // EOF 센티널(0) 감지
+        if now == 0 { return nil }
+        
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리(문제에선 양수지만 일반화)
+        var hasDigit = false
+        while now >= 48, now <= 57 {
+            hasDigit = true
+            sum = sum * 10 + Int(now - 48)
+            now = read()
+        }
+        return hasDigit ? sum * (isPositive ? 1 : -1) : nil
+    }
 }
