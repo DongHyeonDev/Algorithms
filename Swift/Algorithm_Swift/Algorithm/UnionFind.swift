11886745298127e95bb84df1463c996a7b9e5a79
DongHyeonDev <dhkim960206@gmail.com>
2025-08-30 17:14:03 +0900

    Fix: [Swift] Algorithm/UnionFind class화

--- Swift/Algorithm_Swift/Algorithm/UnionFind.swift
+++ Swift/Algorithm_Swift/Algorithm/UnionFind.swift
@@ -6,23 +6,37 @@
 //
 
 // Union-Find 자료구조
-var parent: [Int] = []
-
-@inline(__always) func find(_ x: Int) -> Int {
-    if parent[x] != x {
-        parent[x] = find(parent[x])
-    }
-    return parent[x]
-}
-
-@inline(__always) func union(_ x: Int, _ y: Int) -> Bool {
-    let rootX = find(x)
-    let rootY = find(y)
+class UnionFind {
+    private var parent: [Int]
+    private var rank: [Int]
     
-    if rootX == rootY {
-        return false  // 이미 같은 집합 (사이클 형성)
+    init(_ n: Int) {
+        parent = Array(0..<n)
+        rank = Array(repeating: 0, count: n)
     }
     
-    parent[rootX] = rootY
-    return true  // 성공적으로 합침
+    func find(_ x: Int) -> Int {
+        if parent[x] != x {
+            parent[x] = find(parent[x])
+        }
+        return parent[x]
+    }
+    
+    func union(_ x: Int, _ y: Int) -> Bool {
+        let rootX = find(x)
+        let rootY = find(y)
+        
+        if rootX == rootY { return false }
+        
+        if rank[rootX] < rank[rootY] {
+            parent[rootX] = rootY
+        } else if rank[rootX] > rank[rootY] {
+            parent[rootY] = rootX
+        } else {
+            parent[rootY] = rootX
+            rank[rootX] += 1
+        }
+        
+        return true
+    }
 }
