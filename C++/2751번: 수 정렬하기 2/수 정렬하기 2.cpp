afef3a6353abf1de8b4126b70ce37abd851caa87
DongHyeonDev <dhkim960206@gmail.com>
2025-09-30 16:48:42 +0900

    Feat: [C++] BOJ2751 - 수 정렬하기 2

---
+++ C++/2751번: 수 정렬하기 2/수 정렬하기 2.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 2751                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: dhkim0206 <boj.kr/u/dhkim0206>              +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/2751                           #+#        #+#      #+#    */
+/*   Solved: 2025/09/30 11:59:08 by dhkim0206     ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    int n;
+    std::vector<int> arr;
+    std::cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        int tmp;
+        std::cin >> tmp;
+        arr.push_back(tmp);
+    }
+    
+    std::sort(arr.begin(), arr.end());
+    for (const auto& num : arr) {
+        std::cout << num << '\n';
+    }
+
+    return 0;
+}
