dd01141c1026e38dfea2315a938523160c17318d
DongHyeonDev <dhkim960206@gmail.com>
2025-11-16 15:25:52 +0900

    Feat: [C] BOJ10870 - 피보나치 수 5

--- C/10870번: 피보나치 수 5/피보나치 수 5.c
+++ C/10870번: 피보나치 수 5/피보나치 수 5.c
@@ -1,29 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                      :::    :::    :::     */
-/*   Problem Number: 27433                             :+:    :+:      :+:    */
+/*   Problem Number: 10870                             :+:    :+:      :+:    */
 /*                                                    +:+    +:+        +:+   */
 /*   By: dhkim0206 <boj.kr/u/dhkim0206>              +#+    +#+          +#+  */
 /*                                                  +#+      +#+        +#+   */
-/*   https://boj.kr/27433                          #+#        #+#      #+#    */
-/*   Solved: 2025/11/15 23:43:56 by dhkim0206     ###          ###   ##.kr    */
+/*   https://boj.kr/10870                          #+#        #+#      #+#    */
+/*   Solved: 2025/11/16 14:23:22 by dhkim0206     ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 
-long long factorial(int n) {
-    if (n <= 1) {
-        return 1;
+long long memo[21];
+
+long long fibonacci_memo(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    if (memo[n] != -1) {
+        return memo[n];
     }
 
-    return n * factorial(n - 1);
+    memo[n] = fibonacci_memo(n - 1) + fibonacci_memo(n - 2);
+    return memo[n];
 }
 
 int main() {
     int n;
     scanf("%d", &n);
 
-    printf("%lld\n", factorial(n));
+    for (int i = 0; i <= 20; i++) {
+        memo[i] = -1;
+    }
+
+    printf("%lld\n", fibonacci_memo(n));
 
     return 0;
 }
