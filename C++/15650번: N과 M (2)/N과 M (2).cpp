8a651488e64d3c1d7d4be3bffad51a329d6b10fb
DongHyeonDev <dhkim960206@gmail.com>
2025-11-23 17:36:40 +0900

    Feat: [C++] BOJ15650 - N과 M (2)

---
+++ C++/15650번: N과 M (2)/N과 M (2).cpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 15650                             :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: dhkim0206 <boj.kr/u/dhkim0206>              +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/15650                          #+#        #+#      #+#    */
+/*   Solved: 2025/11/23 17:22:43 by dhkim0206     ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+vector<int> sequence;
+
+void backtrack(int depth, int start) {
+    if (depth == M) {
+        for (int i = 0; i < M; i++) {
+            if (i > 0) cout << " ";
+            cout << sequence[i];
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = start; i <= N; i++) {
+        sequence.push_back(i);
+
+        backtrack(depth + 1, i + 1);
+
+        sequence.pop_back();
+    }
+}
+
+int main() {
+    cin >> N >> M;
+
+    backtrack(0, 1);
+
+    return 0;
+}
