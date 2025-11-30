f943b41de93427762e0e4597c8f95cf122278800
DongHyeonDev <dhkim960206@gmail.com>
2025-11-30 10:42:13 +0900

    Feat: [C++] BOJ24416 - 알고리즘 수업 - 피보나치 수 1

---
+++ C++/24416번: 알고리즘 수업 - 피보나치 수 1/알고리즘 수업 - 피보나치 수 1.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 24416                             :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: dhkim0206 <boj.kr/u/dhkim0206>              +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/24416                          #+#        #+#      #+#    */
+/*   Solved: 2025/11/30 10:29:56 by dhkim0206     ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 동적 프로그래밍으로 피보나치 수 계산
+long long fibonacci(int n) {
+    vector<long long> fib(n + 1);
+    fib[1] = 1;
+    fib[2] = 1;
+
+    for (int i = 3; i <= n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    return fib[n];
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    // 코드1 실행 횟수 = fib(n)
+    long long code1_count = fibonacci(n);
+
+    // 코드2 실행 횟수 = n - 2
+    int code2_count = n - 2;
+
+    cout << code1_count << " " << code2_count << "\n";
+
+    return 0;
+}
