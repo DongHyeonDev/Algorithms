6d11d749e6e5ab9a49a5c059dc634fcc5cfe0aa5
DongHyeonDev <dhkim960206@gmail.com>
2026-01-18 20:32:27 +0900

    Feat: [C++] BOJ24444 - 알고리즘 수업 - 너비 우선 탐색 1

---
+++ C++/24444번: 알고리즘 수업 - 너비 우선 탐색 1/알고리즘 수업 - 너비 우선 탐색 1.cpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 24444                             :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: dhkim0206 <boj.kr/u/dhkim0206>              +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/24444                          #+#        #+#      #+#    */
+/*   Solved: 2026/01/18 15:50:30 by dhkim0206     ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+vector<int> graph[100001];
+int visited[100001];
+int order_count = 1;
+
+void bfs(int start) {
+    queue<int> q;
+    q.push(start);
+    visited[start] = order_count++;
+    
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        
+        for (int next : graph[node]) {
+            if (visited[next] == 0) {
+                visited[next] = order_count++;
+                q.push(next);
+            }
+        }
+    }
+}
+
+int main() {
+    int N, M, R;
+    cin >> N >> M >> R;
+    
+    for (int i = 0; i < M; i++) {
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    
+    // 인접 리스트를 오름차순 정렬
+    for (int i = 1; i <= N; i++) {
+        sort(graph[i].begin(), graph[i].end());
+    }
+    
+    // BFS 시작
+    bfs(R);
+    
+    // 결과 출력
+    for (int i = 1; i <= N; i++) {
+        cout << visited[i] << "\n";
+    }
+    
+    return 0;
+}
