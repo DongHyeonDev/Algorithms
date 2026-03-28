02d863661732bb876fdc6d82a6c1c6d4d9d03e55
DongHyeonDev <dhkim960206@gmail.com>
2026-03-28 15:03:38 +0900

    Feat: [Swift] BOJ20944 - 팰린드롬 척화비

---
+++ Swift/Algorithm_Swift/BOJ/단계별/37. 해 구성하기/20944.swift
@@ -0,0 +1,19 @@
+//
+//  20944.swift
+//  Algorithm_Swift
+//
+//  Created by 김동현 on 3/28/26.
+//
+
+//  문제 링크: https://www.acmicpc.net/problem/20944
+//  알고리즘 분류: 문자열, 애드 혹
+
+class BOJ20944: Solvable {
+    // 메모리: 80488KB, 시간: 52ms, 코드 길이: 119B
+    func run() {
+        // 메인 실행
+        let fileIO = RhynoFileIO()
+        let N = fileIO.readInt()
+        print(String(repeating: "a", count: N))
+    }
+}
